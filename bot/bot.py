acc10406f6732d14205522f5a80184ee36405fea
zombyacoff <zombyacoff@gmail.com>
2026-06-09 11:48:03 +0300

    lint: remove redundant mypy ignore

--- bot/bot.py
+++ bot/bot.py
@@ -14,7 +14,7 @@ class FaceitBot(commands.InteractionBot):
         self.data: faceit.AsyncDataResource | None = None
         super().__init__(**kwargs)
 
-    async def start(self, token: str, **kwargs: Any) -> None:  # type: ignore[override]
+    async def start(self, token: str, **kwargs: Any) -> None:
         async with faceit.AsyncDataResource() as data:
             self.data = data
             await super().start(token, **kwargs)
